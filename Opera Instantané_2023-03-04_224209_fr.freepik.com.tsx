3f26dccac1fee7c1db3f06153a308e067527219f
Nunki0 <samuel.juston@gmail.com>
2023-03-08 08:44:12 -0400

    fichiers inutiles

--- Opera Instantané_2023-03-04_224209_fr.freepik.com.tsx
+++
@@ -1,4 +0,0 @@
-<?xml version="1.0" encoding="UTF-8"?>
-<tileset version="1.9" tiledversion="1.9.2" name="Opera Instantané_2023-03-04_224209_fr.freepik.com" tilewidth="32" tileheight="32" tilecount="1922" columns="62">
- <image source="../Opera Instantané_2023-03-04_224209_fr.freepik.com.png" width="2000" height="1002"/>
-</tileset>
