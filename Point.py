ad0f27b93ea18f10e7e08f9ccc623d4a142fa00a
grymmy97 <maenas972@gmail.com>
2023-04-19 07:36:44 -0400

    animation perso + quelque Point

--- Point.py
+++ Point.py
@@ -1,7 +1,7 @@
 import pygame
-import sprite
+import Sprite
 
-class Point(sprite.Sprites):
+class Point(Sprite.Sprites):
     def __init__(self,x,y):
         super().__init__()
         self.sprite_sheet = pygame.image.load("Point_noir.png").convert_alpha()
--- player.py
+++ player.py
@@ -1,7 +1,8 @@
 import pygame
-import sprite
+import Sprite
+import time
 
-class Explorer(sprite.Sprites):
+class Explorer(Sprite.Sprites):
     def __init__(self,x,y):
         super().__init__()
         self.sprite_sheet = pygame.image.load("player.png").convert_alpha()
@@ -11,32 +12,41 @@ class Explorer(sprite.Sprites):
         self.speed = 4
         self.animation_images = {
             'down':self.get_image(0,0),
-            'left':self.get_image(0,0),
-            'right': self.get_image(0,0),
-            'up': self.get_image(0,0),
+            'right1':self.get_image(0,68),
+            'left2':self.get_image(0,172),
+            'left':self.get_image(0,279),
+            'right2': self.get_image(0,364),
+            'up': self.get_image(0,420),
             'basic' :self.get_image(0,0),
               }
-        
+
     def get_image(self, x=0, y=0):
         """renvoie l'image du sprite"""
-        image = self.create_image(x,y,180,130,self.sprite_sheet)
+        image = self.create_image(x,y,91,71,self.sprite_sheet)
         image.set_colorkey([255,255,255])
         return image
     
     def goto(self,x,y):
         self.rect.center = [x,y]
 
+
     def move_right(self):
         self.goto(self.rect.centerx+self.speed, self.rect.centery)
+        self.image=self.animation_images['right1']
+        
+
 
     def move_left(self):
         self.goto(self.rect.centerx-self.speed, self.rect.centery)
+        self.image = self.animation_images['left']
 
     def move_up(self):
         self.goto(self.rect.centerx, self.rect.centery-self.speed)
+        self.image=self.animation_images['up']
     
     def move_down(self):
         self.goto(self.rect.centerx, self.rect.centery+self.speed)
+        self.image=self.animation_images['down']
 
     
         
